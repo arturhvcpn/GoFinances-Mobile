7ab097d83791303528b5e1856362f0421d567622
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2021-06-04 07:44:39 -0300

    :sparkles: create a new component static transaction card

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 import { 
     Container,
     Header,
@@ -10,7 +11,9 @@ import {
     UserName,
     UserWrapper,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title
 } from './styles';
 
 export function Dashboard(){
@@ -34,6 +37,12 @@ export function Dashboard(){
                 <HighlightCard typeCards='down' title='Saídas' amount='R$ 1.259,00' lastTransaction='Última saída dia 03 de abril'/>
                 <HighlightCard typeCards='total' title='Total' amount='R$ 16.141,00' lastTransaction='01 à 16 de abril'/>
             </HighlightCards>
+            <Transactions>
+                <Title>Listagem</Title>
+                
+                <TransactionCard />
+            
+            </Transactions>
         
         </Container>
     );
